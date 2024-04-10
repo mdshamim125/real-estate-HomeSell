f6b247b710fd302cf827f9d62f2d3b72cdadf027
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-04-10 15:28:02 +0600

    footer almost done

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from "react-router-dom";
 import Home from '../pages/home/Home';
 import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
 const Root = () => {
     return (
         <div className="max-w-6xl  md:mx-auto mx-6 my-10">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
